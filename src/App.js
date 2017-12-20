e08b4ea7689e03236ab04c15df609d5c81779426
urahiroshi <urahiroshi@mercari.com>
2017-12-21 08:48:45 +0900

    Update title and intro

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Playground for create-react-app</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Let's Play!
         </p>
         <button onClick={() => { throw new Error('hogehoge'); }}>Error !</button>
       </div>

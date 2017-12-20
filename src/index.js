1c9342d288ce8ccf61720d49dad0f65982d218a1
urahiroshi <urahiroshi@mercari.com>
2017-12-20 17:33:56 +0900

    Add Sentry Setting

--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// for Sentry
+import Raven from 'raven-js';
+if (process.env.REACT_APP_SENTRY_KEY && process.env.REACT_APP_SENTRY_PROJECT) {
+  console.log('### Configure SENTRY');
+  const key = process.env.REACT_APP_SENTRY_KEY;
+  const project = process.env.REACT_APP_SENTRY_PROJECT;
+  Raven.config(`https://${key}@sentry.io/${project}`).install();
+} else {
+  console.log('### NOT Configure SENTRY');
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
